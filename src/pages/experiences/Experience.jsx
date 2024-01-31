f87aa286b4bad7257430c2d4d224de6754f150dd
makbulhosen5000 <makbulhosen5000@gmail.com>
2024-01-31 12:53:45 +0600

    added education.json file

--- src/pages/experiences/Experience.jsx
+++ src/pages/experiences/Experience.jsx
@@ -6,11 +6,12 @@ const Experience = ({experience}) => {
     return (
         <div className="bg-white p-6 rounded-lg shadow-2xl hover:bg-slate-500">
             <h3 className="text-xl font-bold mb-4"> { experience?.title } </h3>
-            <h3 className="text-xl font-bold mb-4"> { experience?.technology } </h3>
-            <h3 className="text-xl font-bold mb-4"> { experience?.features } </h3>
-            <Link to={experience?.link} target='_blank'>Website || </Link>
-            <Link to={experience?.client} target='_blank'>Clint Code || </Link>
-            <Link to={experience?.server} target='_blank'>Server Code </Link>
+            <h3 className="text-xl font-bold mb-4"> { experience?.period } </h3>
+            <h3 className="text-xl font-bold mb-4"> { experience?.company_name } </h3>
+            <h3 className="text-xl font-bold mb-4"> { experience?.company_address } </h3>
+            <h3 className="text-xl font-bold mb-4"> { experience?.job_type } </h3>
+            <Link to={experience?.website} target='_blank'>Website</Link>
+           
         </div>
     );
 };
