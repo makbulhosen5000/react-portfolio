e822b4342471e818e16c555afa331aa72cc4f5e6
makbulhosen5000 <makbulhosen5000@gmail.com>
2024-02-07 22:55:24 +0600

    updated app id and page id

--- src/component/Messenger/ChatBox.jsx
+++ src/component/Messenger/ChatBox.jsx
@@ -6,8 +6,8 @@ const ChatBox = () => {
     return (
       <div>
         <MessengerCustomerChat
-          pageId="103161434692559"
-          appId="3000396093429747"
+          pageId="118269894702805"
+          appId="1677500379392620"
         />
       </div>
     );
