818254ba59e59c3d80517a8357d9b5f36e4710dc
makbulhosen5000 <makbulhosen5000@gmail.com>
2024-02-18 22:04:53 +0600

    feat:added email

--- src/component/Error/ErrorMessage.jsx
+++ src/component/Error/ErrorMessage.jsx
@@ -6,7 +6,7 @@ const ErrorMessage = () => {
     return (
       <div className="flex flex-col place-items-center justify-center align-items-center h-screen">
         <img width="300" src="https://i.ibb.co/xsfjJt3/not-found.jpg" alt="" />
-        <p>{error.error.message}</p>
+        <p>{error.message}</p>
         <Link to="/">
           <button className="mt-5 bg-yellow-400 rounded-lg w-20 ml-3">HOME</button>
         </Link>
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Lottie from 'lottie-react';
+import emailjs from "@emailjs/browser";
+import {toast} from 'react-hot-toast';
 import developer1 from '../../assets/json/programming-1.json';
 
 const Contact = () => {
+  const form = useRef();
+
+
+  //send email function
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_uiiy1r3",
+        "template_y5hixrr",
+        form.current,
+        //public key
+        "eBs4o8v6cmheTDyjc"
+      )
+      .then(
+        (result) => {
+          toast.success(result.text?"Mail Send Successfully":'');
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
     return (
       <section id="skills" className="bg-gray-200 py-8 mt-8" 
       data-aos="flip-left"
@@ -16,15 +43,15 @@ const Contact = () => {
             <Lottie animationData={developer1} />     
         </div> 
         <div className="mx-auto my-8 p-8 bg-gradient-to-r from-slate-950 to-slate-950  shadow-md rounded-md max-w-xl">
-            <h2 className='text-3xl font-bold text-yellow-300 mb-6' data-aos="fade-up-left">Have a project in your mind, just want to say hi !</h2>
-        <form>
+            <h2 className='text-3xl font-bold text-yellow-300 mb-6' data-aos="fade-up-left">Have a project in your mind ? just want to say hi !</h2>
+        <form ref={form} onSubmit={sendEmail}>
           <div className="mb-4">
             <label htmlFor="name" className="block text-sm font-semibold text-white">Name</label>
-            <input type="text" id="name" name="name" className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-purple-500 text-gray-800" />
+            <input type="text" id="name" name="to_name" className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-purple-500 text-gray-800" />
           </div>
           <div className="mb-4">
             <label htmlFor="email" className="block text-sm font-semibold text-white">Email</label>
-            <input type="email" id="email" name="email" className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-purple-500 text-gray-800" />
+            <input type="email" id="email" name="from_name" className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-purple-500 text-gray-800" />
           </div>
           <div className="mb-6">
             <label htmlFor="message" className="block text-sm font-semibold text-white">Message</label>
@@ -32,7 +59,7 @@ const Contact = () => {
           </div>
           <div className="text-center">
             <button type="submit" className="bg-yellow-300 text-black px-6 py-3 rounded-full font-bold hover:bg-yellow-400 transition duration-300 ease-in-out">Send Message</button>
-          </div>
+          </div>  
         </form>
       </div>
         </div>
