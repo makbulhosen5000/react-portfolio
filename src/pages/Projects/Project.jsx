b52bc923aba9f494f745b9e67abcec22571abb83
makbulhosen5000 <makbulhosen5000@gmail.com>
2024-03-03 20:09:45 +0600

    image link updated

--- src/pages/Projects/Project.jsx
+++ src/pages/Projects/Project.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Project = ({project}) => {
+    const projectImg = "https://i.ibb.co/J7ngxvB/p1.jpg"
 
     return (
-            <div className="text-green-50 bg-cover object-cover bg-center bg-no-repeat p-6 rounded-lg shadow-2xl" style={{ backgroundImage: "url('src/assets/images/projects/p1.jpg')" }}>
+            <div className="text-green-50 bg-cover object-cover bg-center bg-no-repeat p-6 rounded-lg shadow-2xl" style={{ backgroundImage: "url('https://i.ibb.co/J7ngxvB/p1.jpg')" }}>
                 <p className="text-xl font-bold mb-4"> Title: {project?.title} </p>
                 <p className="mb-4"> <span className='font-bold'>Technologies:</span> {project?.technology} </p>
                 <p className="mb-4"> <span className='font-bold'>Features:</span> {project?.features} </p>
