a91ae4f180bde2f5605d79294ef99e4b0b95286e
makbulhosen5000 <makbulhosen5000@gmail.com>
2025-03-08 12:36:17 +0600

    fet:update error page

--- src/component/Error/ErrorMessage.jsx
+++ src/component/Error/ErrorMessage.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { Link, useRouteError } from 'react-router-dom';
 
 const ErrorMessage = () => {
+    const errorImg = 'https://i.ibb.co/xsfjJt3/not-found.jpg';
     const error = useRouteError();
     return (
       <div className="flex flex-col place-items-center justify-center align-items-center h-screen">
-        <img width="300" src="https://i.ibb.co/xsfjJt3/not-found.jpg" alt="" />
+        <img width="300" src={errorImg} alt="" />
         <p>{error.message}</p>
         <Link to="/">
           <button className="mt-5 bg-yellow-400 rounded-lg w-20 ml-3">HOME</button>
